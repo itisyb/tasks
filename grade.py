4e72da209adbe5b04bb59f4755073ff9f25c21e6
itisyb <42640264+itisyb@users.noreply.github.com>
2018-11-04 16:05:13 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,12 +1,12 @@
-length = int(input("Enter number of student:"))
-student ={}
+length = int(input("Enter number of student:"))  #size of students
+student ={} #empty dict
 for i in range(length):
     s = input("Enter student name :")
     eng = int(input("Enter marks in english :"))
     math = int(input("Enter marks in maths :"))
     his = int(input("Enter marks in history :"))
     sci = int(input("Enter marks in science :"))
-    percent = (eng+math+his+sci)/4
+    percent = (eng+math+his+sci)/4 #percentage
     student[s] = {"english":eng,"maths":math,"history":his,"science":sci,"percent":percent}
     print()
     print()
@@ -18,6 +18,8 @@ print()
 print(student)
 print()
 print()
+
+#for finding top scorer in each subject
 print("\t\t Top Scorers ")
 
 subject =  ["english","maths","history","science"]
